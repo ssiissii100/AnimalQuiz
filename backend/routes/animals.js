50fe82fc7eb9524c064b30cc470d8e874d15e4fe
ssiissii100 <sibylle.gehring@hotmail.com>
2023-01-26 21:09:42 +0100

    backend finished

--- backend/routes/animals.js
+++ backend/routes/animals.js
@@ -7,12 +7,87 @@ animalRoutes.route("/speed").get(function (req, res) {
   let db_connect = dbo.getDb("animalquiz");
   db_connect
     .collection("animals")
-    .find({})
+    .find({},{ projection: { name: 1, speed: 1  }}) 
     .toArray(function (err, result) {
       if (err) throw err;
       res.json(result);
     });
 });
- 
+
+animalRoutes.route("/speed").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, speed: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+animalRoutes.route("/").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+animalRoutes.route("/speed").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, speed: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+animalRoutes.route("/weight").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, weight: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+animalRoutes.route("/length").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, length: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+animalRoutes.route("/height").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, height: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+animalRoutes.route("/lifespan").get(function (req, res) {
+  let db_connect = dbo.getDb("animalquiz");
+  db_connect
+    .collection("animals")
+    .find({},{ projection: { name: 1, lifespan: 1  }}) 
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
  
 module.exports = animalRoutes;
