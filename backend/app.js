170b653878a91cddfc2bbf6ad45ea200d47343a7
ssiissii100 <sibylle.gehring@hotmail.com>
2023-01-26 11:31:00 +0100

    backend

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,12 @@ const app = express();
 // Connect Database
 connectDB();
 
+
+
 app.get('/', (req, res) => res.send('Hello world!'));
 app.get('/speed', (req, res) => res.send("Speed"));
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
 
