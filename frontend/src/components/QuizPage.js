52b609d8d666c04b449ecdc2b6066943bdbd70c9
Fabian <fabian.baetscher@lernende.bbw.ch>
2023-01-28 14:27:05 +0100

    quiz done

--- frontend/src/components/QuizPage.js
+++ frontend/src/components/QuizPage.js
@@ -1,34 +1,98 @@
 import React, { useState, useEffect } from "react";
-import round from "./Results";
-import { handleNextRound } from "./Quiz";
 
 const QuizPage = ({ animals, selectedCategory, onCorrectAnswer }) => {
   const [selectedAnimals, setSelectedAnimals] = React.useState(null);
+  const [AnimalsList, setAnimalsList] = useState(null);
+  const [round, setRound] = useState(1);
+  const [points, setPoints] = useState(0);
+  const [weiter, setWeiter] = useState(false);
+  const [Answered, setAnswered] = useState(false);
+  const [Richtig, setRichtig] = useState("");
+  const [disabled, setDisabled] = useState(false);
 
   const handleAnswerSelected = (e) => {
     setSelectedAnimals(e.target.value);
+    setAnswered(true);
+  };
+
+  const pickRandom = (arr) => {
+    let randomArr = [...arr];
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+      let randomIndex = Math.floor(Math.random() * randomArr.length);
+      result.push(randomArr[randomIndex]);
+      randomArr.splice(randomIndex, 1);
+    }
+    return result;
+  };
+
+  const getAnimals = () => {
+    setAnimalsList(pickRandom(animals));
+    setWeiter(false);
+    setRichtig("");
+    setRound(round + 1);
+    setDisabled(false);
+  };
+  const start = () => {
+    setAnimalsList(pickRandom(animals));
   };
 
   const handleSubmit = () => {
-    // animals[selectedCategory];
+    console.log(AnimalsList);
+    console.log(AnimalsList);
+    console.log(selectedCategory);
+    setDisabled(true);
+
+    let highestLength = 0;
+    let animalName = "";
+    AnimalsList.forEach((item) => {
+      if (highestLength < item[selectedCategory]) {
+        highestLength = item[selectedCategory];
+        animalName = item.name;
+      }
+    });
+    console.log(highestLength, animalName);
+
+    if (selectedAnimals === animalName) {
+      setPoints(points + 1);
+      setWeiter(true);
+      setRichtig("Richtig");
+      setAnswered(false);
+    } else {
+      setWeiter(true);
+      setRichtig("Falsch");
+      setAnswered(false);
+    }
   };
+
+  if (round > 5) {
+    console
+      .log
+      //go to Auswertung
+      ();
+  }
 
   return (
     <div>
-      {/* <h2>Round {round}</h2> */}
+      <h2>Round {round}/5</h2>
       <p>Select the animal with the highest {selectedCategory}</p>
-      {animals?.map((animal, index) => (
+      {AnimalsList === null ? <button onClick={start}>start</button> : " "}
+      {AnimalsList?.map((animal, index) => (
         <div key={index}>
           <input
             type="radio"
             value={animal.name}
             name="animals"
             onChange={handleAnswerSelected}
+            disabled={disabled}
           />
           <label>{animal.name}</label>
         </div>
       ))}
-      <button onClick={handleSubmit}>Submit</button>
+      <p>{Richtig}</p>
+
+      {Answered ? <button onClick={handleSubmit}>Submit</button> : " "}
+      {weiter ? <button onClick={getAnimals}>Weiter</button> : " "}
     </div>
   );
 };
